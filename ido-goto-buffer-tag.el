06f2058b027b01c5b7332a08509c7f0d5f89b2d6
Joel Nises <joel.nises@sciss.se>
2012-03-19 08:35:53 +0100

    updated documentation

--- ido-goto-buffer-tag.el
+++ ido-goto-buffer-tag.el
@@ -1,5 +1,21 @@
+;;; -*- lexical-binding: t -*-
+
+;;; ido-goto-buffer-tag.el --- go to a semantic or imenu buffer tag in the current buffer using ido
+
 ;; Author: Joel Nises
-;; to use do something like:
+;; Version: 1.1
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;;; Usage
 ;; (add-to-list 'load-path "~/.emacs.d/el/ido-goto-buffer-tag/")
 ;; (when (require 'ido-goto-buffer-tag nil t) (global-set-key (kbd "C-c i") 'ido-goto-buffer-tag))
 
